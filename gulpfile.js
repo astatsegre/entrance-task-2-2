8c81046db2a9ec2db8d5e4de49e72b09ea5d5a10
stas <astatsegre@gmail.com>
2018-07-29 16:36:18 +0300

    main layout

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,14 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
-const concat = require('gulp-concat');
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/*.scss')
+gulp.task('sass', () => {
+  return gulp.src('./sass/style.scss')
     .pipe(sass())
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
     }))
     .pipe(cleanCss())
-    .pipe(concat('style.css'))
     .pipe(gulp.dest('./css'))
 });
 
