c9942bd5afe6d4c5ca0d02c39d3b76e73bf01c96
stas <astatsegre@gmail.com>
2018-07-29 21:22:07 +0300

    header

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
+const svgSprite = require("gulp-svg-sprites");
 
 gulp.task('sass', () => {
   return gulp.src('./sass/style.scss')
@@ -10,7 +11,19 @@ gulp.task('sass', () => {
       browsers: ['last 2 versions']
     }))
     .pipe(cleanCss())
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('svg', () => {
+  gulp.src('./images/*.svg')
+    .pipe(svgSprite({
+      cssFile: "../sass/_sprite.scss",
+      preview: false,
+      svg: {
+        sprite: "images/_sprite.svg"
+      }
+    }))
+    .pipe(gulp.dest('./images'));
 });
 
 gulp.task('sass:watch', () => {
