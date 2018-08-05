087e38eb034b93c97ae1dbb9bd1f533e525c23dc
stas <astatsegre@gmail.com>
2018-08-06 00:35:27 +0300

    open animation

--- js/index.js
+++ js/index.js
@@ -4,17 +4,38 @@ window.onload = function () {
 
 function toggleModal(event) {
   let cardForAnimation = document.createElement('div');
-  cardForAnimation.classList.add('card');
-  cardForAnimation.classList.add('card--tall');
-  event.target.appendChild(cardForAnimation)
-  console.log(event.target)
+  let overlay = document.getElementById('js-overlay');
+  let layout = document.getElementById('js-layout');
+  let accept = document.getElementById('js-modal-accept');
+  let cancel = document.getElementById('js-modal-cancel');
 
-  let modal = document.getElementById('js-modal');
+  cardForAnimation.classList.add('anim-card');
+  // cardForAnimation.classList.add('modal');
+  cardForAnimation.style.top = event.currentTarget.getBoundingClientRect().top + 'px';
+  cardForAnimation.style.left = event.currentTarget.getBoundingClientRect().left + 'px';
+  // cardForAnimation.style.width = event.currentTarget.getBoundingClientRect().width + 'px';
+  // cardForAnimation.style.height = event.currentTarget.getBoundingClientRect().height + 'px';
+  document.body.appendChild(cardForAnimation)
+  let requestId = requestAnimationFrame(function () {
+    cardForAnimation.style.top = '20px';
+    cardForAnimation.style.left = '20px';
+    cardForAnimation.classList.add('modal')
+    overlay.classList.add('overlay')
+    layout.classList.add('gaussian')
+  });
+  setTimeout(function () {
+    accept.classList.remove('modal__btn--none')
+    cancel.classList.remove('modal__btn--none')
+  }, 400)
+
+  console.log(event.currentTarget.getBoundingClientRect())
+
+ /* let modal = document.getElementById('js-modal');
   let modalWindow = document.getElementById('js-modal-window');
   let layout = document.getElementById('js-layout');
   modal.classList.toggle('modal--none');
   modal.classList.toggle('modal--hidden');
-  console.log(222, modalWindow.getBoundingClientRect())
+  console.log(222, modalWindow.getBoundingClientRect())*/
 
   // layout.classList.toggle('gaussian');
 }
