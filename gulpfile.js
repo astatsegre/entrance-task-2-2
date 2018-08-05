5f0a3905f76ccdc22da0b94271f6ce4196e69d8c
stas <astatsegre@gmail.com>
2018-08-05 11:39:42 +0300

    svg sprite

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,22 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
-const svgSprite = require("gulp-svg-sprites");
+const svgSprite = require("gulp-svg-sprite");
+
+const config = {
+  mode: {
+    css: {
+      render: {
+        scss: {dest: "_sprite.scss"}
+      },
+      sprite: '../images/svg/_sprite',
+      dest: '../sass'
+    }
+  },
+  shape: {
+    spacing: {padding: 1}
+  }
+};
 
 gulp.task('sass', () => {
   return gulp.src('./sass/style.scss')
@@ -15,15 +30,9 @@ gulp.task('sass', () => {
 });
 
 gulp.task('svg', () => {
-  gulp.src('./images/*.svg')
-    .pipe(svgSprite({
-      cssFile: "../sass/_sprite.scss",
-      preview: false,
-      svg: {
-        sprite: "images/_sprite.svg"
-      }
-    }))
-    .pipe(gulp.dest('./images'));
+  gulp.src('./images/svg/*.svg')
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest('./sass'));
 });
 
 gulp.task('sass:watch', () => {
