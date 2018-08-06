869c59ea444c19820860d92d1d4673e5bf62d9a0
stas <astatsegre@gmail.com>
2018-08-06 17:19:40 +0300

    close-open animation refactoring

--- js/index.js
+++ js/index.js
@@ -3,68 +3,59 @@ window.onload = function () {
 };
 
 function openModal(event, id) {
-  let cardForAnimation = document.createElement('div');
-  let overlay = document.getElementById('js-overlay');
-  let layout = document.getElementById('js-layout');
-  let modal = document.getElementById(`js-modal-window-${id}`);
-  let accept = document.getElementById(`js-modal-accept-${id}`);
-  let cancel = document.getElementById(`js-modal-cancel-${id}`);
-  let cardTop = event.currentTarget.getBoundingClientRect().top + 'px';
-  let cardLeft = event.currentTarget.getBoundingClientRect().left + 'px';
+  let {accept, cancel, cardForAnimation, cardLeft, cardTop, layout, modal, overlay} = varsFactory(id);
 
   cardForAnimation.classList.add('anim-card');
-  cardForAnimation.style.top = cardTop;
-  cardForAnimation.style.left = cardLeft;
-  document.body.appendChild(cardForAnimation)
-  let requestId = requestAnimationFrame(function () {
-    cardForAnimation.style.top = '20px';
-    cardForAnimation.style.left = '20px';
-    cardForAnimation.classList.add('modal')
-    overlay.classList.add('overlay')
+  cardForAnimation.style.cssText = `top: ${cardTop}; left: ${cardLeft}`;
+  document.body.appendChild(cardForAnimation);
+
+  requestAnimationFrame(function () {
+    cardForAnimation.style.cssText = `top: 20px; left: 20px`;
+    cardForAnimation.classList.add('modal');
+    overlay.classList.add('overlay');
     layout.classList.add('gaussian')
   });
+
   setTimeout(function () {
-    accept.classList.remove('modal__btn--none')
-    cancel.classList.remove('modal__btn--none')
-    modal.classList.remove('modal--none')
+    accept.classList.remove('modal__btn--none');
+    cancel.classList.remove('modal__btn--none');
+    modal.classList.remove('modal--none');
     cardForAnimation.remove()
   }, 400)
 }
 
 function closeModal(event, id) {
-  let cardForAnimation = document.createElement('div');
-  let modal = document.getElementById(`js-modal-window-${id}`);
-  let card = document.getElementById(`js-card-id-${id}`);
-  let cardTop = card.getBoundingClientRect().top + 'px';
-  let cardLeft = card.getBoundingClientRect().left + 'px';
-  let cardBackgroundColor = window.getComputedStyle(card, null).backgroundColor;
-  let accept = document.getElementById(`js-modal-accept-${id}`);
-  let cancel = document.getElementById(`js-modal-cancel-${id}`);
-  let overlay = document.getElementById('js-overlay');
-  let layout = document.getElementById('js-layout');
+  let {accept, cancel, cardBackgroundColor, cardForAnimation, cardLeft, cardTop, layout, modal, overlay} = varsFactory(id);
 
   cardForAnimation.classList.add('modal');
-  document.body.appendChild(cardForAnimation)
+  document.body.appendChild(cardForAnimation);
+  modal.classList.add('modal--none');
 
-  accept.classList.add('modal__btn--none')
-  cancel.classList.add('modal__btn--none')
-  overlay.classList.remove('overlay')
-  layout.classList.remove('gaussian')
+  accept.classList.add('modal__btn--none');
+  cancel.classList.add('modal__btn--none');
+  overlay.classList.remove('overlay');
+  layout.classList.remove('gaussian');
 
-  let requestId = requestAnimationFrame(function () {
-    cardForAnimation.style.top = cardTop;
-    cardForAnimation.style.left = cardLeft;
-    cardForAnimation.style.width = '200px';
-    cardForAnimation.style.height = '120px';
-    cardForAnimation.style.backgroundColor = cardBackgroundColor;
-
+  requestAnimationFrame(function () {
+    cardForAnimation.style.cssText = `top: ${cardTop}; left: ${cardLeft}; width: 200px; height: 120px; background-color: ${cardBackgroundColor}`;
   });
 
   setTimeout(function () {
     cardForAnimation.remove()
   }, 400)
+}
 
-
-  modal.classList.add('modal--none')
-
+function varsFactory(id) {
+  let card = document.getElementById(`js-card-id-${id}`);
+  return {
+    accept: document.getElementById(`js-modal-accept-${id}`),
+    cancel: document.getElementById(`js-modal-cancel-${id}`),
+    cardBackgroundColor: window.getComputedStyle(card, null).backgroundColor,
+    cardForAnimation: document.createElement('div'),
+    cardLeft: card.getBoundingClientRect().left + 'px',
+    cardTop: card.getBoundingClientRect().top + 'px',
+    layout: document.getElementById('js-layout'),
+    modal: document.getElementById(`js-modal-window-${id}`),
+    overlay: document.getElementById('js-overlay'),
+  }
 }
